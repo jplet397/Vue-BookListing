f2432523a3a2e4190f4430069a28e4948a3fe6c9
Joris Plettinx <Joris.Plettinx@realdolmen.com>
2018-12-16 04:05:53 +0100

    module3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,14 +2,20 @@
 <div>
   <h1>{{title}}</h1>
   <ul>
-    <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    <book-item v-for="book in books" :book='book'></book-item>
   </ul>
 </div>
 </template>
 
 <script>
+  import BookItem from './BookItem';
+
   export default {
     name: 'BookList',
+    components: { BookItem },
+    comments: {
+      BookItem
+    },
     data(){
       return {
         title: 'All Books',
@@ -21,7 +27,6 @@
       }
     }
   }
-
 </script>
 
 <style>
